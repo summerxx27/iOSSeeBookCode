0692e79485ece5b99cd2bdf8290b637048cf9840
77KMeng <soi_otf@163.com>
2016-08-03 11:05:36 +0800

    update
    update

--- iOS编程实战_01/ViewController.m
+++ iOS编程实战_01/ViewController.m
@@ -44,49 +44,88 @@ static CATransform3D MakePerspectiveTransform()
     perspective.m34 = -1. / 2000.;
     return perspective;
 }
-
+- (CALayer *)layerx:(CGFloat)x
+                  y:(CGFloat)y
+                  z:(CGFloat)z
+              color:(UIColor *)color
+          transform:(CATransform3D)transform
+{
+    CALayer *layer = [CALayer layer];
+    layer.backgroundColor = [color CGColor];
+    layer.bounds = CGRectMake(0, 0, 100, 100);
+    layer.position = CGPointMake(x, y);
+    layer.zPosition = z;
+    layer.transform = transform;
+    return layer;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     /// 分类中load方法的作用, 类似于一般的init方法可以对分类进行一些设置(例如: 初始化静态变量), load方法只会调用一次, 不应该手动调用, , 勇士也不需要调用 [super load]
     
-    CATransform3D transform;
-    // 1
-    transform = CATransform3DMakeTranslation(0, -kSize / 2, 0);
-    transform = CATransform3DRotate(transform, M_PI_2, 1.0, 0, 0);
-    self.topLayer = [self layerWithColor:[UIColor purpleColor] transform:transform];
-    [self.view.layer addSublayer:self.topLayer];
-    // 2
-    transform = CATransform3DMakeTranslation(0, kSize / 2, 0);
-    transform = CATransform3DRotate(transform, M_PI_2, 1.0, 0, 0);
-    self.bottomLayer = [self layerWithColor:[UIColor blackColor] transform:transform];
-    [self.view.layer addSublayer:self.bottomLayer];
-    // 3
-    transform = CATransform3DMakeTranslation(kSize / 2, 0, 0);
-    transform = CATransform3DRotate(transform, M_PI_2, 0, 1.0, 0);
-    self.leftLayer = [self layerWithColor:[UIColor lightGrayColor] transform:transform];
-    [self.view.layer addSublayer:self.leftLayer];
-    // 4
-    transform = CATransform3DMakeTranslation(-kSize / 2, 0, 0);
-    transform = CATransform3DRotate(transform, M_PI_2, 0, 1.0, 0);
-    self.rightLayer = [self layerWithColor:[UIColor blueColor] transform:transform];
-    [self.view.layer addSublayer:self.rightLayer];
-    // 5
-    transform = CATransform3DMakeTranslation(0, 0, -kSize / 2);
-    transform = CATransform3DRotate(transform, M_PI_2, 0, 0, 0);
-    self.frontLayer = [self layerWithColor:[UIColor greenColor] transform:transform];
-    [self.view.layer addSublayer:self.frontLayer];
-    // 6
-    transform = CATransform3DMakeTranslation(0, 0, kSize / 2);
-    transform = CATransform3DRotate(transform, M_PI_2, 0, 0, 0);
-    self.backLayer = [self layerWithColor:[UIColor redColor] transform:transform];
-    [self.view.layer addSublayer:self.backLayer];
-
+//    CATransform3D transform;
+//    // 1
+//    transform = CATransform3DMakeTranslation(0, -kSize / 2, 0);
+//    transform = CATransform3DRotate(transform, M_PI_2, 1.0, 0, 0);
+//    self.topLayer = [self layerWithColor:[UIColor purpleColor] transform:transform];
+//    [self.view.layer addSublayer:self.topLayer];
+//    // 2
+//    transform = CATransform3DMakeTranslation(0, kSize / 2, 0);
+//    transform = CATransform3DRotate(transform, M_PI_2, 1.0, 0, 0);
+//    self.bottomLayer = [self layerWithColor:[UIColor blackColor] transform:transform];
+//    [self.view.layer addSublayer:self.bottomLayer];
+//    // 3
+//    transform = CATransform3DMakeTranslation(kSize / 2, 0, 0);
+//    transform = CATransform3DRotate(transform, M_PI_2, 0, 1.0, 0);
+//    self.leftLayer = [self layerWithColor:[UIColor lightGrayColor] transform:transform];
+//    [self.view.layer addSublayer:self.leftLayer];
+//    // 4
+//    transform = CATransform3DMakeTranslation(-kSize / 2, 0, 0);
+//    transform = CATransform3DRotate(transform, M_PI_2, 0, 1.0, 0);
+//    self.rightLayer = [self layerWithColor:[UIColor blueColor] transform:transform];
+//    [self.view.layer addSublayer:self.rightLayer];
+//    // 5
+//    transform = CATransform3DMakeTranslation(0, 0, -kSize / 2);
+//    transform = CATransform3DRotate(transform, M_PI_2, 0, 0, 0);
+//    self.frontLayer = [self layerWithColor:[UIColor greenColor] transform:transform];
+//    [self.view.layer addSublayer:self.frontLayer];
+//    // 6
+//    transform = CATransform3DMakeTranslation(0, 0, kSize / 2);
+//    transform = CATransform3DRotate(transform, M_PI_2, 0, 0, 0);
+//    self.backLayer = [self layerWithColor:[UIColor redColor] transform:transform];
+//    [self.view.layer addSublayer:self.backLayer];
+//    self.view.layer.sublayerTransform = MakePerspectiveTransform();
+//    UIGestureRecognizer *g = [[UIPanGestureRecognizer alloc] initWithTarget:self action: @selector(pan:)];
+//    [self.view addGestureRecognizer:g];
+//    
+//    UIView *view = [UIView new];
+//    view.frame = CGRectMake(20, 20, 50, 50);
+//    [self.view addSubview:view];
+//    view.backgroundColor = [UIColor cyanColor];
+//    // 显式动画
+//    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+//    anim.fromValue = @1.0;
+//    anim.toValue = @0.0;
+//    anim.autoreverses = YES;
+//    anim.repeatCount = INFINITY;
+//    [view.layer addAnimation:anim forKey:@"anim"];
+//    
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        //
+//        [view.layer removeAnimationForKey:@"anim"];
+//    });
     
-    self.view.layer.sublayerTransform = MakePerspectiveTransform();
     
-    UIGestureRecognizer *g = [[UIPanGestureRecognizer alloc] initWithTarget:self action: @selector(pan:)];
-    [self.view addGestureRecognizer:g];
+    // 3.
+    
+    CATransformLayer *contentLayer = [CATransformLayer layer];
+    contentLayer.frame = self.view.frame;
+    CGSize size = contentLayer.bounds.size;
+    contentLayer.transform = CATransform3DMakeTranslation(size.width / 2, size.height / 2, 0);
+    [self.view.layer addSublayer:contentLayer];
+    
+    self.topLayer = [self layerx:0 y:-50 z:0 color:[UIColor cyanColor] transform:MakeSideRoration(1,0,0)];
+    
 }
 - (void)pan:(UIPanGestureRecognizer *)rec
 {
